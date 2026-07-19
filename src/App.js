9bd71769599bd98061a141ccdafda45a25243f88
alif <alifmaulana312@gmail.com>
2026-07-19 22:42:55 +0700

    feat: change content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          INI DEV
+          INI PROD
         </p>
         <a
           className="App-link"
